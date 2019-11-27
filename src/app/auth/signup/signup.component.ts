b4091f3a97badc15fefd74cc7f4dc73ad879ba22
ak672676 <ak672676@gmail.com>
2019-11-27 16:40:54 +0530

    Final Project

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { AuthService } from "../auth.service";
+import { Subscription } from "rxjs";
+
+@Component({
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.css"]
+})
+export class SignupComponent implements OnInit, OnDestroy {
+  isLoading = false;
+  private authStatusSub: Subscription;
+  constructor(public authService: AuthService) {}
+  ngOnInit() {
+    this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe(authStatus => {
+        this.isLoading = false;
+      });
+  }
+  onSignup(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.isLoading = true;
+    //console.log(form.value.email);
+    this.authService.createUser(form.value.email, form.value.password);
+    //this.isLoading = false;
+  }
+  ngOnDestroy() {
+    this.authStatusSub.unsubscribe();
+  }
+}
